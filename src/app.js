e98f517925d805c56cdb7c83ef0f1f1a86b0036f
Federico Plastina <40448913+fgplastina@users.noreply.github.com>
2025-06-28 22:32:08 -0300

    feat: envvar para el host

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(cors());
 app.use(express.json()); // parsea JSON
 
 // TODO:Rutas
+app.use(express.static('public'));
+app.use('/products', productRoutes);
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import app from './app.js';
 import environments from './config/environments.js';
 
 const PORT = environments.port || 3000;
+const HOST = environments.host || 'http://localhost';
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en ${HOST}:${PORT}`);
 });
