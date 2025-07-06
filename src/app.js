37f3c84ac4002eb85b8473c78acdb6620f4867fa
Federico Plastina <40448913+fgplastina@users.noreply.github.com>
2025-07-06 19:27:34 -0300

    re estructuración de app.js + dependencias

--- src/app.js
+++ src/app.js
@@ -1,33 +1,49 @@
 import express from 'express';
 import cors from 'cors';
-import productRoutes from './routes/productRoutes.js';
-import backofficeRoutes from './routes/backofficeRoutes.js';
+import session from 'express-session';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import productRoutes from './routes/productRoutes.js';
+import backofficeRoutes from './routes/backofficeRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import { requireAdmin } from './middlewares/authMiddleware.js';
+
 const app = express();
 
-// Middlewares
-app.use(cors());
-app.use(express.json()); // parsea JSON
-
-// TODO:Rutas
-app.use(express.static('public'));
-app.use('/products', productRoutes);
-
+// Ruta base del proyecto (necesaria con ES Modules)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+/*
+ *  Middlewares globales
+ */
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(
+  session({
+    secret: 'clave_secreta_segura',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//  Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*
- *  Configuración de EJS views
+ *  Rutas
+ */
+app.use('/products', productRoutes); // API
+app.use('/', authRoutes); // Login / logout
+app.use('/backoffice', requireAdmin, backofficeRoutes); // Admin protegido
+
+/*
+ *  Configuración del motor de vistas EJS
  */
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-app.use('/backoffice', backofficeRoutes);
 
 export default app;
